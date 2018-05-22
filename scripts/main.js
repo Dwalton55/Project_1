816a4a5640f7f501a5780a61438766236a51f5d3
Dwalton55 <deonte.e.walton@gmail.com>
2018-05-22 12:57:24 -0400

    added logic to control the turns on the game and automate the process of switching turns

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@ console.log("connected")
 
 console.log('Inside connected')
 
+
+let counter = 0
+
 let newgame = {
     score: 0,
     level: 6,
@@ -17,46 +20,63 @@ let level = newgame.level
 let playerArry = newgame.playerArry
 
 let startgame = {
-welcome : ()=>{
-    //display welcom message on message html
-},
-RandomGen: ()=> {
-   cpuArray.push( Math.floor(Math.random() * 4))
+    welcome: () => {
+        //display welcom message on message html
+    },
+    RandomGen: () => {
+        cpuArray.push(Math.floor(Math.random() * 4))
 
-},
+    },
 }
 
 
 
 turnTracker = {
-    cpuTurn : true,
-    playerTurn : false,
+    cpuTurn: true,
+    playerTurn: false,
     //not finished in here yet
-    switcher : ()=> {
-        if(this.cpuTurn === true){
+    switcher: () => {
+        if (this.cpuTurn === true) {
             this.playerTurn = false
             Turns.cpuTurn
-        } else{
-            if(this.cpuTurn === false){
+        } else {
+            if (this.cpuTurn === false) {
                 playerTurn = true
             }
         }
     }
 }
 
-
+let playerTurn = turnTracker.playerTurn
 let switcher = turnTracker.switcher
 
+
 let Turns = {
-    cpuTurn : ()=> {
+    cpuTurn: () => {
         startgame.RandomGen()
-        //update the display
+        console.log(cpuArray) //update the display
+        console.log('end turn')
+        turnTracker.cpuTurn = false
+        turnTracker.playerTurn =true
     },
 
-    playerTurn : ()=> {
+    playerTurn: () => {
         // input large function. will check player array against cpu array after every click
         // click > check array > increase click counter > turn checker > countinue or end turn
-        
+        if (playerArry[counter] === cpuArray[counter]) {
+            counter++
+            if(playerArry.length === cpuArray.length){
+                //increase level
+                // end player turn
+                turnTracker.cpuTurn = true
+                turnTracker.playerTurn = false
+                this.cpuTurn()
+            } 
+        } else {
+            console.log("try again")
+            //reset to level one
+            // call a new game
+        }
     }
 }
 
@@ -66,23 +86,38 @@ let domHande = {
 }
 
 //let red = ()=>{
-    $('#red').on('click',()=>{
-        if()
+$('#red').on('click', () => {
+    if (playerTurn) {
         console.log('clicked')
-       playerArry.push(1) 
-    })
-    $('#blue').on('click',()=>{
+        playerArry.push(1)
+    } else {
+        console.log(" not your turn!!!")
+    }
+})
+$('#blue').on('click', () => {
+    if (playerTurn) {
         console.log('clicked')
-       playerArry.push(2) 
-    })
-    $('#green').on('click',()=>{
+        playerArry.push(2)
+    } else {
+        console.log(" not your turn!!!")
+    }
+})
+$('#green').on('click', () => {
+    if (playerTurn) {
         console.log('clicked')
-       playerArry.push(3) 
-    })
-    $('#yellow').on('click',()=>{
+        playerArry.push(3)
+    } else {
+        console.log(" not your turn!!!")
+    }
+})
+$('#yellow').on('click', () => {
+    if (playerTurn) {
         console.log('clicked')
-       playerArry.push(4) 
-    })
+        playerArry.push(4)
+    } else {
+        console.log(" not your turn!!!")
+    }
+})
 //}
 
 
