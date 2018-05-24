ecde64515c743d04b79c15563c8437f7e5d4ac55
Dwalton55 <deonte.e.walton@gmail.com>
2018-05-24 13:19:01 -0400

    added styles, hated it

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,18 @@
 console.log("connected")
-
-
-console.log('Inside connected')
-
-
 let counter = 0
-
 let game = {
     level: 0,
     playerArry: [],
     cpuArray: [],
     score: 0,
     hardmode: false,
-    difficulty: 1, // increase by level times 100
+    difficulty: 1, // increase by level times 
     RandomGen: () => {
         cpuArray.push(Math.floor(Math.random() * 5))
 
     },
     newgame: () => {
-        
+
         game.level = 1
         game.playerArry = []
         game.cpuArray = []
@@ -36,41 +30,35 @@ let game = {
     musicmode: { // begin object
         playerMusicarray: [],
         musicPlay: false,
-        musicMode: ()=>{
+        musicMode: () => {
             console.log("calling musicmode")
             console.log("music mode on")
             $('.rotateMessage').html('I guess relaxing is good too. <br> Let me know when you are ready to play')
-            $('.textarea').html('Lets relax and hear you play')
+            $('.text').html('Lets relax and hear you play')
             musicPlay = true
             turnTracker.cpuTurn = false
             playerTurn = false
         },
-        playsong: ()=>{
-            if(game.musicmode.musicPlay){
-            domHande.flash(game.musicmode.playerMusicarray)
+        playsong: () => {
+            if (game.musicmode.musicPlay) {
+                domHande.flash(game.musicmode.playerMusicarray)
             }
         },
-        clear: ()=>{
-            game.musicmode.playerMusicarray =[]
+        clear: () => {
+            game.musicmode.playerMusicarray = []
         },
     } // end object
 
 
 }
-
 console.log("test")
 let cpuArray = game.cpuArray
 let playerArry = game.playerArry
-
 turnTracker = { // push into game?
     cpuTurn: true,
     playerTurn: false,
 }
-
 let playerTurn = turnTracker.playerTurn
-
-
-
 let Turns = {
     cpuTurn: () => { // function start
         turnTracker.cpuTurn = true
@@ -116,8 +104,8 @@ let Turns = {
                 Turns.cpuTurn()
             }
         } else {
-            
-            $('.textarea').html('Wrong Button. <br> Click new game to try again')
+
+            $('.text').html('Wrong Button. <br> Click new game to try again')
             $('.rotateMessage').html('Womp womp womp')
             cpuArray = []
             turnTracker.playerTurn = false
@@ -126,19 +114,17 @@ let Turns = {
         }
     }
 }
-
 let endGame = {
     winning: 8,
     highscore: 3,
     hscalc: () => {
         console.log('testing')
         if (game.level > endGame.highscore) {
-            $('.textarea').html("New HighScore!!!!")
+            $('.text').html("New HighScore!!!!")
             endGame.highscore = game.level
         }
     },
 }
-
 let domHande = {
     updateDisplay: () => {
         $('.level').html(game.level)
@@ -146,83 +132,82 @@ let domHande = {
         $('.highscore').html(endGame.highscore)
     },
     flash: (colors) => {
-        
+
         colors.forEach((number, index) => {
             console.log(index + "time through")
             if (number === 0) {
                 setTimeout(() => {
                     $(".red").toggleClass("redlight");
                     audio.red()
-                }, ((index * 2) + 1) * (250* game.difficulty));
+                }, ((index * 2) + 1) * (1000 * game.difficulty));
                 setTimeout(() => {
                     $(".red").toggleClass("redlight");
-                }, (index * 2) * (250* game.difficulty));
+                }, (index * 2) * (1000 * game.difficulty));
                 console.log('flashing red')
 
             } else if (number === 1) {
                 setTimeout(() => {
                     $(".blue").toggleClass("bluelight");
                     audio.blue()
-                }, ((index * 2) + 1) * (250* game.difficulty));
+                }, ((index * 2) + 1) * (1000 * game.difficulty));
 
                 setTimeout(() => {
                     $(".blue").toggleClass("bluelight");
-                }, (index * 2) * (250* game.difficulty));
+                }, (index * 2) * (1000 * game.difficulty));
                 console.log('flashing blue')
 
             } else if (number === 2) {
                 setTimeout(() => {
                     $(".green").toggleClass("greenlight");
                     audio.green()
-                }, ((index * 2) + 1) * (250* game.difficulty));
+                }, ((index * 2) + 1) * (1000 * game.difficulty));
 
                 setTimeout(() => {
                     $(".green").toggleClass("greenlight");
-                }, (index * 2) * (250* game.difficulty));
+                }, (index * 2) * (1000 * game.difficulty));
                 console.log('flashing green')
 
             } else if (number === 3) {
                 setTimeout(() => {
                     $(".yellow").toggleClass("yellowlight");
                     audio.yellow()
-                }, ((index * 2) + 1) * (250* game.difficulty));
+                }, ((index * 2) + 1) * (1000 * game.difficulty));
 
                 setTimeout(() => {
                     $(".yellow").toggleClass("yellowlight");
-                }, (index * 2) * (250* game.difficulty));
+                }, (index * 2) * (1000 * game.difficulty));
                 console.log('flashing yellow')
             } else if (number === 4) {
                 setTimeout(() => {
                     $(".orange").toggleClass("orangelight");
                     audio.orange()
-                }, ((index * 2) + 1) * (250* game.difficulty));
+                }, ((index * 2) + 1) * (1000 * game.difficulty));
 
                 setTimeout(() => {
                     $(".orange").toggleClass("orangelight");
-                }, (index * 2) * (250* game.difficulty));
+                }, (index * 2) * (1000 * game.difficulty));
                 console.log('flashing orange')
             }
         });
     },
     welcomeMessage: () => {
-        $('.textarea').html('welcome to the game!')
+        $('.text').html('welcome to the game!')
     },
     lose: () => {
-        $('.textarea').html('Nice try Try again')
+        $('.text').html('Nice try Try again')
     },
     inround: () => {
-        $('.textarea').html("You Leveled Up! try to keep up")
+        $('.text').html("You Leveled Up! try to keep up")
     },
     simeon: () => {
-        $('.textarea').html("Now its my turn!!")
+        $('.text').html("Now its my turn!!")
     },
     win: () => {
-        $('.textarea').html("You won!! you are the best!!")
+        $('.text').html("You won!! you are the best!!")
         $('.rotateMessage').html('Noooo YOU BEAT ME!!!!<br>Lets go again!')
     }
 
 }
-
 let audio = {
     red: () => {
         document.getElementById('red').play()
@@ -240,8 +225,6 @@ let audio = {
         document.getElementById('orange').play()
     },
 }
-
-
 $('.red').on('click', (e) => {
     audio.red()
     if (playerTurn) {
@@ -250,7 +233,7 @@ $('.red').on('click', (e) => {
         console.log(playerArry)
         Turns.playerTurn()
 
-    } else if(game.musicmode.musicPlay){
+    } else if (game.musicmode.musicPlay) {
         console.log("music mode red")
         game.musicmode.playerMusicarray.push(0)
     } else {
@@ -264,9 +247,9 @@ $('.blue').on('click', () => {
         playerArry.push(1)
         console.log(playerArry)
         Turns.playerTurn()
-    }else if(game.musicmode.musicPlay){
+    } else if (game.musicmode.musicPlay) {
         game.musicmode.playerMusicarray.push(1)
-    }else {
+    } else {
         console.log(" not your turn!!!")
     }
 })
@@ -277,9 +260,9 @@ $('.green').on('click', () => {
         playerArry.push(2)
         console.log(playerArry)
         Turns.playerTurn()
-    }else if(game.musicmode.musicPlay){
+    } else if (game.musicmode.musicPlay) {
         game.musicmode.playerMusicarray.push(2)
-    }else {
+    } else {
         console.log(" not your turn!!!")
     }
 })
@@ -290,9 +273,9 @@ $('.yellow').on('click', () => {
         playerArry.push(3)
         console.log(playerArry)
         Turns.playerTurn()
-    }else if(game.musicmode.musicPlay){
+    } else if (game.musicmode.musicPlay) {
         game.musicmode.playerMusicarray.push(3)
-    }else {
+    } else {
         console.log(" not your turn!!!")
     }
 })
@@ -303,7 +286,7 @@ $('.orange').on('click', () => {
         playerArry.push(4)
         console.log(playerArry)
         Turns.playerTurn()
-    }else if(game.musicmode.musicPlay){
+    } else if (game.musicmode.musicPlay) {
         game.musicmode.playerMusicarray.push(4)
     } else {
         console.log(" not your turn!!!")
@@ -316,32 +299,32 @@ $('.newgame').on('click', () => {
     game.newgame()
 })
 $('.hardmode').on('click', () => {
-    if(!game.hardmode ){
+    if (!game.hardmode) {
         game.hardmode = true
         game.difficulty = .5
-        $('.textarea').html('welcome to the game! you have selected hard mode')
+        $('.text').html('welcome to the game! you have selected hard mode')
         $('.rotateMessage').html('You must be confident!')
 
     } else {
         game.hardmode = false
         game.difficulty = 1
-        $('.textarea').html('welcome to the game! you are on easy mode')
+        $('.text').html('welcome to the game! you are on easy mode')
         $('.rotateMessage').html("Guess I'll go easy on you")
     }
-    
+
 })
 
-$('.musicmode').on('click',()=>{
+$('.musicmode').on('click', () => {
     game.musicmode.musicMode()
     console.log("music init")
     game.musicmode.musicPlay = true
     console.log(game.musicmode.musicPlay)
-    
+
 })
-$('.play').on('click', ()=>{
+$('.play').on('click', () => {
     console.log('click recived')
     game.musicmode.playsong()
 })
-$('.clear').on('click',()=>{
+$('.clear').on('click', () => {
     game.musicmode.clear()
 })
